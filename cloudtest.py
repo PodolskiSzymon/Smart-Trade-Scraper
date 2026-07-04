bd148688529ec617ceadd3ec7a893d8885811375
Szymon Podolski <podolszymon@gmail.com>
2026-07-04 14:02:35 +0200

    fixed google cloudtest

--- cloudtest.py
+++ cloudtest.py
@@ -1,28 +1,25 @@
 from google.cloud import storage
 
-def wyslij_do_chmury(nazwa_bucketa, sciezka_lokalna, nazwa_docelowa):
-    # Tworzymy klienta, który automatycznie znajdzie token logowania z terminala
-    klient = storage.Client()
+def wyslij_do_chmury(nazwa_bucketa, sciezka_lokalna, nazwa_docelowa, id_projektu):
+    # TERAZ KLIENT WIE, DO JAKIEGO PROJEKTU SIĘ PODŁĄCZYĆ
+    klient = storage.Client(project=id_projektu)
     
-    # Łączymy się z Twoim wiaderkiem
     wiaderko = klient.bucket(nazwa_bucketa)
-    
-    # Przygotowujemy "puste miejsce" w chmurze
     plik_w_chmurze = wiaderko.blob(nazwa_docelowa)
     
-    # Wysyłamy plik
     print(f"Wysyłam plik z {sciezka_lokalna}...")
     plik_w_chmurze.upload_from_filename(sciezka_lokalna)
     print("Sukces! Zdjęcie jest już na serwerach Google.")
 
 if __name__ == "__main__":
-    # Nazwa Twojego zasobnika utworzonego przed chwilą
-    NAZWA_ZASOBNIKA = "smart-trade-scraper"
+    # Identyfikator Twojego projektu (skopiowany z Twojej konsoli)
+    MOJ_PROJEKT = "project-6ffdf12f-c10d-4003-8bf"
     
-    # ZMIEŃ TO: ścieżka do jakiegoś fizycznego zdjęcia na dysku F:
-    PLIK_Z_DYSKU = r"F:\WEBSCRAPER\jakies_zdjecie.jpg" 
+    NAZWA_ZASOBNIKA = "smart_trade_scraper"
     
-    # Gdzie to ma trafić (od razu tworzymy wirtualny folder 01_landing_zone)
+    # Upewnij się, że podajesz prawdziwą ścieżkę do pliku, np.:
+    PLIK_Z_DYSKU = r"F:\WEBSCRAPER\dataset_yolo\images\train\zdj1.png" 
+    
     MIEJSCE_W_CHMURZE = "01_landing_zone/moje_pierwsze_zdjecie.jpg"
     
-    wyslij_do_chmury(NAZWA_ZASOBNIKA, PLIK_Z_DYSKU, MIEJSCE_W_CHMURZE)
+    wyslij_do_chmury(NAZWA_ZASOBNIKA, PLIK_Z_DYSKU, MIEJSCE_W_CHMURZE, MOJ_PROJEKT)
